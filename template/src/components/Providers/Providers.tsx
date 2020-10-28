39d871cc81f19090b253855777be03702394794c
Sergio Herrera Guzmán <sergio@sergiohgz.eu>
2020-10-29 00:42:47 +0100

    feat: add Emotion as theming library

--- template/src/components/Providers/Providers.tsx
+++ template/src/components/Providers/Providers.tsx
@@ -1,13 +1,14 @@
 import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
+import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
 import React, { FC } from 'react';
 
 import theme from '../../config/theme';
 
 const Providers: FC = ({ children }) => (
-    <>
+    <EmotionThemeProvider theme={theme}>
         <CssBaseline />
         <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
-    </>
+    </EmotionThemeProvider>
 );
 
 export default Providers;
